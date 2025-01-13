52588161d5b02b5d719154c73d4fe9beac8a066f
color.ui <rarnab225@gmail.com>
2025-01-13 15:19:00 +0530

    Version Upgrade

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
-import './main.css'
+import './main.css';
 
-const hamburger=document.querySelector('.hamburger-menu');
-const navbarMenu=document.querySelector('.navBar-menu');
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const navBarMenu = document.querySelector('.navBar-menu');
 
-hamburger.addEventListener('click',()=>{
-    navbarMenu.classList.toggle('hide');
+    if (hamburgerMenu && navBarMenu) {
+        hamburgerMenu.addEventListener('click', function() {
+            navBarMenu.classList.toggle('hide');
+            navBarMenu.classList.toggle('show');
+        });
+    }
+
+    function animateValue(id, start, end, duration) {
+        const obj = document.getElementById(id);
+        const range = end - start;
+        let startTime = null;
+
+        function step(timestamp) {
+            if (!startTime) {
+                startTime = timestamp;
+                obj.classList.add("fade-in"); // Add fade-in class when animation starts
+            }
+            const progress = Math.min((timestamp - startTime) / duration, 1);
+            obj.innerText = Math.floor(progress * range + start) + " +";
+            if (progress < 1) {
+                window.requestAnimationFrame(step);
+            }
+        }
+        window.requestAnimationFrame(step);
+    }
+
+    animateValue("clients", 0, 300, 2000);
+    animateValue("projects", 0, 900, 2000);
+    animateValue("awards", 0, 20, 2000);
+
 });
